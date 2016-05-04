ad57083560a96af97e7c3511fc1aec4ceb335142
Brian Nivens <briannivens@gmail.com>
2016-05-03 22:43:28 -0400

    <master> Completes tip.js

--- tips.js
+++ tips.js
@@ -1,1 +1,9 @@
+alert("Welcome to the tip calculator!");
 var bill = prompt("Enter the bill amount:");
+if(confirm("All tips are 20% of the bill")){
+    var tip = bill * 0.20;
+    var billWithTip = Number(bill) + tip;
+    alert("Bill: " + bill + ", Tip: " + tip + ", New Bill: " + billWithTip)
+  }else{
+    alert("Please be kind to your servers.")
+  }
